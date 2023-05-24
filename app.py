6b2085e97edc4a69b0a8e2af940b5a86dbc44515
lenisestevez <105001209+lenisrepositorios@users.noreply.github.com>
2023-05-24 12:06:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import json
 
 app=Flask(__name__)
 
@@ -7,14 +7,19 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/procesar_contenido',methods = ['POST'])
+
+def guardarCodigo():
+    data = request.get_json()
+    content = data['content']
+    # Realiza cualquier procesamiento necesario con el contenido del textarea
+    resultado = procesar_contenido(content)
+    return resultado
+
+def procesar_contenido(content):
+    # Realiza aquí tu lógica de procesamiento del contenido
+    # Por ejemplo, puedes simplemente devolver el contenido en mayúsculas
+    return content.upper()
+
 if __name__=='__main__':
     app.run(debug=True)
-
-
-@app.route('/guardar-contenido',methods = ['POST'])
-def guardarCodigo():
-    contenido = request.form.get("contenido")
-    return "Contenido guardado correctamente"
-
-
-
